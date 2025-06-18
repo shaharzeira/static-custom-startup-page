9e84e2d0bd37a35cd3e6ebb1c123c61474f7ffc8
shuh <shahar.zeira@gmail.com>
2025-06-18 21:55:09 +0300

    .

---
+++ data.js
@@ -0,0 +1,37 @@
+// Link data (originally from links.json)
+const linksData = [
+  [
+    {
+      label: "Google",
+      image: "images/google.png",
+      url: "https://www.google.com"
+    },
+    {
+      label: "ChatGPT",
+      image: "images/chatgpt.svg",
+      url: "https://chat.openai.com"
+    },
+    {
+      label: "Albums",
+      image: "images/photos.png",
+      url: "https://photos.google.com/albums"
+    },
+    {
+      label: "Gmail",
+      image: "https://ssl.gstatic.com/ui/v1/icons/mail/rfr/gmail.ico",
+      url: "https://mail.google.com"
+    },
+  ],
+  [
+    {
+      label: "DeepSeek",
+      image: "images/deepseek.ico",
+      url: "https://chat.deepseek.com"
+    },
+    {
+      label: "Trending Repos",
+      image: "images/github.svg",
+      url: "https://github.com/trending"
+    }
+  ]
+];
