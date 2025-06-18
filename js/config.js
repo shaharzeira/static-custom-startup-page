718d84d5abfc6509037256a77d5b6b491eb66dc3
shuh <shahar.zeira@gmail.com>
2025-06-19 01:04:20 +0300

    clean code

--- js/config.js
+++ js/config.js
@@ -1,12 +1,2 @@
-const CARD_WIDTH_CLASS = 'w-32';
-
-function getRemFromTailwindWidth(className) {
-  const match = className.match(/^w-(\d+)$/);
-  if (match) {
-    const twUnits = parseInt(match[1], 10);
-    return `${twUnits / 4}rem`;
-  }
-  throw new Error(`Unsupported class: ${className}`);
-}
-
-const CARD_WIDTH_REM = getRemFromTailwindWidth(CARD_WIDTH_CLASS);
+const CARD_WIDTH_REM = '8rem'
+const CARD_WIDTH_CLASS = `!w-[${CARD_WIDTH_REM}]`; // Tailwind will accept arbitrary values with []
