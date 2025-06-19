ab0b056745ba63211cc8f72697fdd3c21f48f785
shuh <shahar.zeira@gmail.com>
2025-06-19 16:22:04 +0300

    build tailwind v3.3.5

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    ".\\*.html",
+    ".\\*.js",
+    ".\\js\\*.js",
+    ".\\tailwind\\*.css"
+  ],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms')({ strategy: 'class' }),
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+}
