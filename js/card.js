5cc18f6b9626d8f74b1a871a2242c5513e48cdcf
shuh <shahar.zeira@gmail.com>
2025-08-11 10:20:05 +0300

    increase cards size

--- js/card.js
+++ js/card.js
@@ -5,14 +5,14 @@ function createCard(link) {
   card.href = link.url;
   card.target = '_blank';
 
-  card.className = `${CARD_WIDTH_CLASS} bg-white py-1 px-1 rounded-lg shadow-[0_1px_2px_rgba(0,0,0,0.3)] 
+  card.className = `${CARD_WIDTH_CLASS} bg-white py-2 px-1 rounded-lg shadow-[0_1px_2px_rgba(0,0,0,0.3)] 
   transition transform hover:-translate-y-1 flex 
   ${USE_VERTICAL_LAYOUT ? 'flex-col space-y-1 text-center' : 'items-center space-x-4'} items-center justify-center`;
 
   const img = document.createElement('img');
   img.src = link.image;
   img.alt = link.label;
-  img.className = 'h-10 object-contain';
+  img.className = 'h-14 object-contain';
 
   const span = document.createElement('span');
   span.textContent = link.label;
