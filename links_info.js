f446d63a92d142288c553478f4a69e4155a63b34
shuh <shahar.zeira@gmail.com>
2025-08-17 12:15:20 +0300

    Update links_info.js

--- links_info.js
+++ links_info.js
@@ -2,34 +2,34 @@ const linksInfo = [
   [
     {
       label: "Google",
-      image: "images/g.png",
+      image: "links-images/g.png",
       url: "https://www.google.com"
     },
     {
       label: "ChatGPT",
-      image: "images/chatgpt.svg",
+      image: "links-images/chatgpt.svg",
       url: "https://chat.openai.com"
     },
     {
       label: "Albums",
-      image: "images/photos.png",
+      image: "links-images/photos.png",
       url: "https://photos.google.com/albums"
     },
     {
       label: "Gmail",
-      image: "images/gmail.ico",
+      image: "links-images/gmail.ico",
       url: "https://mail.google.com"
     },
   ],
   [
     {
       label: "DeepSeek",
-      image: "images/deepseek.ico",
+      image: "links-images/deepseek.ico",
       url: "https://chat.deepseek.com"
     },
     {
       label: "Trending Repos",
-      image: "images/github.svg",
+      image: "links-images/github.svg",
       url: "https://github.com/trending"
     }
   ]
